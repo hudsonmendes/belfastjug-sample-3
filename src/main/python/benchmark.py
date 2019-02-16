a3d07c4587323c2ce98f58d41eec5108756972cc
Hudson Mendes <hudsonmendes@gmail.com>
2019-02-16 20:54:24 +0000

    compararison

--- src/main/python/benchmark.py
+++ src/main/python/benchmark.py
@@ -1,30 +1,32 @@
 import numpy
 from datetime import datetime
 from tabulate import tabulate
+from colorama import Fore, Style
 
-MAX_ROWS = 1000
-MAX_COLUMNS = 10
-GP = 10
+DIMENSIONS = [
+    { 'rows':       100, 'columns': 10 },
+    { 'rows':      1000, 'columns': 10 },
+    { 'rows':     10000, 'columns': 10 },
+    { 'rows':    100000, 'columns': 10 },
+    { 'rows':   1000000, 'columns': 10 },
+    { 'rows':  10000000, 'columns': 10 },
+]
 
 def main():
     print('running tests...')
     results = []
-    rows = 1
-    columns = 1
-    while rows <= MAX_ROWS and columns <= MAX_COLUMNS:
-        dimensions = { 'rows': rows, 'columns': columns }
+    for dimensions in DIMENSIONS:
+        benckmark_dimensions(dimensions)
         results.append(benchmark(dimensions))
-        columns *= GP
-        rows *= GP
     print('tabulating results...')
-    print(tabulate(results, headers=benchmark_header(), tablefmt='presto'))
+    benchmark_print(results)
     
 def matrix_add_serial(a, b, dimensions):
     columns = dimensions['columns']
     rows = dimensions['rows']
-    ret = [[0] * columns] * rows
+    ret = numpy.zeros((rows, columns))
     for i in range(0, rows):
-        for j in range(1, columns):
+        for j in range(0, columns):
             ret[i][j] = a[i][j] + b[i][j]
     return ret
     
@@ -34,11 +36,16 @@ def matrix_add_vector(a, b, dimensions):
 def benchmark_header():
     return [
         "dimensions",
-        "elapsedms_serial",
-        "elapsedms_vector",
+        "elapsed_in_ms (serial)",
+        "elapsed_in_ms (vector)",
+        "faster vector",
         "checksum",
         "match"
     ]
+    
+def benckmark_dimensions(dimensions):
+    dimensions_as_s = f'{dimensions["rows"]} x {dimensions["columns"]}'
+    print(f'benchmarking: {dimensions_as_s.rjust(13)}')
         
 def benchmark(dimensions):
     a = numpy.random.randn(dimensions['rows'], dimensions['columns'])
@@ -47,13 +54,22 @@ def benchmark(dimensions):
     timed_vector = timed(lambda: matrix_add_vector(a, b, dimensions))
     checksum_serial = checksum(timed_serial['result'], dimensions)
     checksum_vector = checksum(timed_vector['result'], dimensions)
+    faster_in_ms = timed_vector["elapsed_in_ms"] - timed_serial["elapsed_in_ms"]
     
     return [
-        f'm: {str(dimensions["columns"]).rjust(3)} x n: {str(dimensions["rows"]).rjust(5)}',
+        f'{str(dimensions["rows"])} x {str(dimensions["columns"])}',
         f'{timed_serial["elapsed_in_ms"]}ms',
         f'{timed_vector["elapsed_in_ms"]}ms',
+        green(f'{faster_in_ms}ms'),
         checksum_serial,
         "✅" if checksum_serial == checksum_vector else "🔴" ]
+    
+def benchmark_print(results):
+    print(tabulate(
+        results,
+        tablefmt='presto',
+        colalign=('right', 'right', 'right', 'right',),
+        headers=benchmark_header(),))
 
 def checksum(vector, dimensions):
     columns = dimensions['columns']
@@ -70,4 +86,8 @@ def timed(proc):
     toc = datetime.now()
     elapsed_in_ms = int((toc - tic).microseconds / 1000)
     return { 'result': ret, 'elapsed_in_ms': elapsed_in_ms }
+
+def green(exp):
+    return Fore.GREEN + exp + Style.RESET_ALL
+
 main()
